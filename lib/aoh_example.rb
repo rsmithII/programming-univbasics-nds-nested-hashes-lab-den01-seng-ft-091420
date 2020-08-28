e0d9e7f1a9dd7fbf2d1264bfeb6a7091fb7c3daf
Rodney Smith II <r_smith3@hotmail.com>
2020-08-28 21:15:56 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -34,7 +34,7 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   aoh[1][:name] = "Joelle van Dyne (PGOAT)"
-  aoh[row][key]
+  aoh
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
